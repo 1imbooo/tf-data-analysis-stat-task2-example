391b3af302fdf4e0585e40ec360d9474d9568c3c
1imbooo <130186620+1imbooo@users.noreply.github.com>
2023-04-08 03:06:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = 2*x.mean()/(59**2)
+    loc = (2 * np.ones_like(x))*x.mean()/(59**2)
     scale = np.sqrt((4 / (59 ** 4))*np.var(x)) / np.sqrt(len(x))
     return loc - scale * norm.ppf(1 - alpha / 2), \
            loc - scale * norm.ppf(alpha / 2)
